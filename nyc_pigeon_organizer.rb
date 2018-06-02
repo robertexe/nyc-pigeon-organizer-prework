238db3e2d60df34bc829851f6f3ba48695cb5164
Robert Tabares <robtabares92@gmail.com>
2018-06-02 16:22:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do | att, hash |
+    hash.each do | details, names |
+      names.each do | name |
+
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {att => [details.to_s]}
+        elsif pigeon_list[name][att] == nil
+          pigeon_list[name].merge!({att => [details.to_s]})
+        else
+          pigeon_list[name][att] << details.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
